a7652a6c7b61f94f3fb2a119003cb1938cf051c6
semioticsolutions <k.polak@semiotyka.com>
2025-04-30 12:10:43 +0200

    Update index.tsx

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -663,7 +663,7 @@ const Main: FC<IMainProps> = () => {
           </div>
         )}
         {/* main */}
-        <div className='flex-grow flex flex-col h-[calc(100vh_-_3rem)] overflow-y-auto' style={{ display: 'none' }}>
+        <div className='flex-grow flex flex-col h-[calc(100vh_-_3rem)] overflow-y-auto'>
           <ConfigSence
             conversationName={conversationName}
             hasSetInputs={hasSetInputs}
